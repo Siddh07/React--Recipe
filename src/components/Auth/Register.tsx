a59c6b82c62fb16511245fa4df260bea438ce691
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-08-21 23:39:06 +0545

    Login Form with supabase

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
-import { supabase } from '../../supabaseeClient';
+import { supabase } from '../../supabaseClient';
 
-const Register: React.FC = () => {
-    const [email, setEmail] = useState('');
+const Register: React.FC = () => { //Define a functional component for the registration form
+    const [email, setEmail] = useState(''); //Declare state for email, updated by setEmail
     const [password, setPassword] = useState('');
   
-    const handleSubmit = async (e: React.FormEvent) => {
-      e.preventDefault();
+    const handleSubmit = async (e: React.FormEvent) => { //Function to handle form submission and user registration
+      e.preventDefault(); // prevent reload after form submission
       try {
-        const { data, error }: { data: any, error: any } = await supabase.auth.signUp({
-          email,
+        const { data, error }: { data: any, error: any } = await supabase.auth.signUp({  
+            // Destructure(Extract values and assign) the response from supabase.auth.signUp into data and error variables
+            email,
           password,
         });
-        if (error) throw error;
+        if (error) throw error; //if an error occurs, throw it and handle it in the catch block
         alert("User registered successfully!");
-      } catch (error: any) {
+      } catch (error: any) { //Catch and log any errors that occur during the registration process
         console.error("Error during registration:", error.message);
       }
     };
   
     return (
       <form onSubmit={handleSubmit}>
+        {/* When the form is submitted, the handleSubmit function is triggered */}
         <input
           type="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)} 
+        //   Update the email state with the current input value
           placeholder="Email"
           required
         />
